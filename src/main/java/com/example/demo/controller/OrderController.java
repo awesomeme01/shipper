15365c4b4cf132aa5aa9f193b592f8cd30fb985c
Shabdan <shabdan.abjaparov,official@gmail.com>
2021-08-05 04:47:51 +0600

    added password changing

--- src/main/java/com/example/demo/controller/OrderController.java
+++ src/main/java/com/example/demo/controller/OrderController.java
@@ -25,55 +25,55 @@ public class OrderController {
     @Secured("ROLE_USER")
     @PostMapping("/create")
     public Response create(Principal principal, @RequestBody OrderWrapper wrapper){
-        try{
+//        try{
             Order order = new Order();
             order.setDescription(wrapper.getDescription());
             order.setPriceFromInvoice(wrapper.getPriceFromInvoice());
             order.setTrackNumber(wrapper.getTrackNumber());
             order.setUser(userService.getByUsername(principal.getName()));
             return new Response(true, "Order Successfuly created!", orderService.create(order));
-        }catch (Exception ex){
-            return new Response(false, "Unexpected error!", ex.getMessage());
-        }
+//        }catch (Exception ex){
+//            return new Response(false, "Unexpected error!", ex.getMessage());
+//        }
     }
     @Secured("ROLE_ADMIN")
     @PostMapping("/updateStatus/{orderId}")
     public Response updateStatus(@PathVariable Long orderId, @RequestBody OrderStatusWrapper orderStatusWrapper){
-        try{
+//        try{
             return new Response(true, "Order status changed to " +orderStatusWrapper.getOrderStatus()+"!", orderService.updateStatus(orderId,orderStatusWrapper.getOrderStatus()));
-        }catch (Exception ex){
-            return new Response(false, "Unexpected error!", ex.getMessage());
-        }
+//        }catch (Exception ex){
+//            return new Response(false, "Unexpected error!", ex.getMessage());
+//        }
     }
     @Secured({"ROLE_ADMIN", "ROLE_USER"})
     @PostMapping("/update")
     public Response update (@RequestBody Order order){
-        try{
+//        try{
             return new Response(true, "Order with id = " + order.getId() + " has been updated!", orderService.update(order));
-        }catch (Exception ex){
-            return new Response(false,"Unexpected error!", ex.getMessage());
-        }
+//        }catch (Exception ex){
+//            return new Response(false,"Unexpected error!", ex.getMessage());
+//        }
     }
     @Secured("ROLE_ADMIN")
     @DeleteMapping("/delete/{id}")
     public Response deleteByID (@PathVariable Long id){
-        try{
+//        try{
             orderService.delete(id);
             return new Response(true, "Order has been deleted!", null);
-        }catch (Exception ex){
-            return new Response(false,"Unexpected error!", ex.getMessage());
-        }
+//        }catch (Exception ex){
+//            return new Response(false,"Unexpected error!", ex.getMessage());
+//        }
     }
     @Secured("ROLE_USER")
     @GetMapping("/getMyOrders")
     public Response getMyOrders(Principal principal){
-        try{
+//        try{
             User user = userService.getByUsername(principal.getName());
             return new Response(true, "All orders of the current user!", orderService.getMyOrders(user));
-        }
-        catch (Exception ex){
-            return new Response(false,"Unexpected error!", ex.getMessage());
-        }
+//        }
+//        catch (Exception ex){
+//            return new Response(false,"Unexpected error!", ex.getMessage());
+//        }
     }
 
 
