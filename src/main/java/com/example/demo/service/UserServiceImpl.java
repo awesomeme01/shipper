a7174f5d758de6f586631984be9801adb83fa7b9
Shabdan <46133469+awesomeme01@users.noreply.github.com>
2021-08-16 01:28:46 +0600

    Update UserServiceImpl.java

--- src/main/java/com/example/demo/service/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/UserServiceImpl.java
@@ -10,7 +10,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Random;
-import jabs.util.List;
+import java.util.List;
 
 
 @Service
